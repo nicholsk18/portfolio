dd8436e5187d2e3007bd764ea2084366402558ea
Karson Nichols <nicholsk18@gmail.com>
2020-10-21 10:51:40 -0500

    finished styling form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -31,20 +31,26 @@ const Contact = () => {
           <div className={animate_line}></div>
           <Label id={name_label} label="name" />
         </div>
+
         <div className={form_group}>
           <Input id={email} type="email" />
+          <div className={animate_line}></div>
           <Label id={email_label} label="email" />
         </div>
+
         <div className={form_group}>
           <Input id={subject} type="text" />
+          <div className={animate_line}></div>
           <Label id={subject_label} label="subject" />
         </div>
+
         <div className={form_group}>
           <Textarea
             id={message}
             name="message"
             // placeholder="Enter your message here..."
           />
+          <div className={animate_line}></div>
           <Label id={message_label} label="Enter your message here..." />
         </div>
       </form>
