695a04776a6395921d88a44e9f91acbe21278f1b
Karson <nicholsk18@gmail.com>
2020-10-22 21:44:59 -0500

    added about layout

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
+import {
+  about_container,
+  about_img,
+  about_text,
+} from '../../public/assets/css/about.module.css'
 
 const About = () => {
   return (
@@ -10,13 +15,44 @@ const About = () => {
       </Head>
       <PageHeader text="About Me" />
 
-      <div>
-        <div>
-          <h2></h2>
-          <p></p>
+      <div className={about_container}>
+        <div className={about_text}>
+          <h2>Place Holder Text</h2>
+          <p>
+            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+            ipscumlorim ipscumlorim ipscum
+          </p>
         </div>
-        <div>
+
+        <div className={about_img}>
           <img src="/assets/images/thinking-no-bg.png" alt="" />
+        </div>
+      </div>
+
+      <div className={about_container}>
+        <div className={about_img}>
+          <img src="/assets/images/version_control-no-bg.png" alt="" />
+        </div>
+        <div className={about_text}>
+          <h2>Place Holder Text</h2>
+          <p>
+            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+            ipscumlorim ipscumlorim ipscum
+          </p>
+        </div>
+      </div>
+
+      <div className={about_container}>
+        <div className={about_text}>
+          <h2>Place Holder Text</h2>
+          <p>
+            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+            ipscumlorim ipscumlorim ipscum
+          </p>
+        </div>
+
+        <div className={about_img}>
+          <img src="/assets/images/programming-no-bg.png" alt="" />
         </div>
       </div>
     </Layout>
