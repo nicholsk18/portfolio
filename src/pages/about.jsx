0aa0be858207daac6ff9e2ea105477da43efaf79
Karson <nicholsk18@gmail.com>
2020-10-22 22:41:56 -0500

    working on about section, twiking height

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,8 @@ import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
 import {
   about_container,
+  about_section,
+  reverse,
   about_img,
   about_text,
 } from '../../public/assets/css/about.module.css'
@@ -16,43 +18,45 @@ const About = () => {
       <PageHeader text="About Me" />
 
       <div className={about_container}>
-        <div className={about_text}>
-          <h2>Place Holder Text</h2>
-          <p>
-            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
-            ipscumlorim ipscumlorim ipscum
-          </p>
+        <div className={about_section}>
+          <div className={about_text}>
+            <h2>Place Holder Text</h2>
+            <p>
+              lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+              ipscumlorim ipscumlorim ipscum
+            </p>
+          </div>
+
+          <div className={about_img}>
+            <img src="/assets/images/thinking-no-bg.png" alt="" />
+          </div>
         </div>
 
-        <div className={about_img}>
-          <img src="/assets/images/thinking-no-bg.png" alt="" />
-        </div>
-      </div>
-
-      <div className={about_container}>
-        <div className={about_img}>
-          <img src="/assets/images/version_control-no-bg.png" alt="" />
-        </div>
-        <div className={about_text}>
-          <h2>Place Holder Text</h2>
-          <p>
-            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
-            ipscumlorim ipscumlorim ipscum
-          </p>
-        </div>
-      </div>
-
-      <div className={about_container}>
-        <div className={about_text}>
-          <h2>Place Holder Text</h2>
-          <p>
-            lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
-            ipscumlorim ipscumlorim ipscum
-          </p>
+        <div className={`${about_section} ${reverse}`}>
+          <div className={about_img}>
+            <img src="/assets/images/version_control-no-bg.png" alt="" />
+          </div>
+          <div className={about_text}>
+            <h2>Place Holder Text</h2>
+            <p>
+              lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+              ipscumlorim ipscumlorim ipscum
+            </p>
+          </div>
         </div>
 
-        <div className={about_img}>
-          <img src="/assets/images/programming-no-bg.png" alt="" />
+        <div className={about_section}>
+          <div className={about_text}>
+            <h2>Place Holder Text</h2>
+            <p>
+              lorim ipscum lorim ipscumlorim ipscumlorim ipscumlorim ipscumlorim
+              ipscumlorim ipscumlorim ipscum
+            </p>
+          </div>
+
+          <div className={about_img}>
+            <img src="/assets/images/programming-no-bg.png" alt="" />
+          </div>
         </div>
       </div>
     </Layout>
