4be6d062979a69fa201819c1ff5a05ed9990febd
Karson Nichols <nicholsk18@gmail.com>
2020-10-23 10:47:11 -0500

    refractor form. Took out components and changed to regular fields. Changed animation so it will stay on top after input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,48 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
     animate_line,
     contact_form,
-    email,
     form_group,
+    label_active,
     input_label,
-    message,
-    name,
-    subject
+    input_name,
+    input_email,
+    input_subject,
+    input_message,
+    submit_button
 } from "../../public/assets/css/input.module.css";
 
 const Form = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
+
+    // If user put input in change the class name
+    // this way animation will stay up
+    const checkActive = (inputLength) => {
+        return inputLength > 0 ? label_active : input_label
+    }
+
     return (
         <form id={contact_form} action="">
             <div className={form_group}>
-                <input id={name} type="text" name={name} required />
+                <input id={input_name} type="text" name={input_name} onChange={(e) => setName(e.target.value)} required />
                 <div className={animate_line}></div>
-                <label className={input_label} htmlFor="name">
+                <label className={checkActive(name.length)} htmlFor="name">
                     Name
                 </label>
             </div>
 
             <div className={form_group}>
-                <input id={email} type="email" name={email} required />
+                <input id={input_email} type="email" name={input_email} onChange={(e) => setEmail(e.target.value)} required />
                 <div className={animate_line}></div>
-                <label className={input_label} htmlFor="email">
+                <label className={checkActive(email.length)} htmlFor="email">
                     Email
                 </label>
             </div>
 
             <div className={form_group}>
-                <input id={subject} type="text" name={subject} required />
+                <input id={input_subject} type="text" name={input_subject} onChange={(e) => setSubject(e.target.value)} required />
                 <div className={animate_line}></div>
-                <label className={input_label} htmlFor="subject">
+                <label className={checkActive(subject.length)} htmlFor="subject">
                     Subject
                 </label>
             </div>
 
             <div className={form_group}>
-                <textarea id={message} name={message}></textarea>
+                <textarea id={input_message} name={input_message} onChange={(e) => setMessage(e.target.value)}>
+                </textarea>
                 <div className={animate_line}></div>
-                <label className={input_label} htmlFor="textarea">
+                <label className={checkActive(message.length)} htmlFor="textarea">
                     Enter your message here...
                 </label>
+            </div>
+
+            <div className={submit_button}>
+                <button>Submit</button>
             </div>
         </form>
     )
