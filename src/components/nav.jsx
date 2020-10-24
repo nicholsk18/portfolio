3d1c5919384621ff146f045ce5e55ef067ef5cc6
Karson <nicholsk18@gmail.com>
2020-10-23 23:45:15 -0500

    responsive fixes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import ActiveLink from "./ActiveLink";
+import ActiveLink from './ActiveLink'
 import {
   navbar,
   nav_item,
@@ -7,38 +7,64 @@ import {
   nav_sub_title,
   nav_link,
   active,
+  nav_mobile,
 } from '../../public/assets/css/nav.module.css'
 
 const Nav = () => {
-
   return (
-    <nav id={navbar}>
-      <h2 id={nav_title}>Karson Nichols</h2>
-      <p id={nav_sub_title}>Web Developer</p>
+    <>
+      <nav id={navbar}>
+        <h2 id={nav_title}>Karson Nichols</h2>
+        <p id={nav_sub_title}>Web Developer</p>
 
-      <div id={nav_item}>
-        <ActiveLink activeClassName={active} href="/">
-          <a className={nav_link}>Home</a>
-        </ActiveLink>
+        <div id={nav_item}>
+          <ActiveLink activeClassName={active} href="/">
+            <a className={nav_link}>Home</a>
+          </ActiveLink>
 
-        <ActiveLink activeClassName={active} href="/projects">
-          <a className={nav_link}>Projects</a>
-        </ActiveLink>
+          <ActiveLink activeClassName={active} href="/projects">
+            <a className={nav_link}>Projects</a>
+          </ActiveLink>
 
-        <ActiveLink activeClassName={active} href="/skills">
-          <a className={nav_link}>Skills</a>
-        </ActiveLink>
+          <ActiveLink activeClassName={active} href="/skills">
+            <a className={nav_link}>Skills</a>
+          </ActiveLink>
 
-        <ActiveLink activeClassName={active} href="/about">
-          <a className={nav_link}>About Me</a>
-        </ActiveLink>
+          <ActiveLink activeClassName={active} href="/about">
+            <a className={nav_link}>About Me</a>
+          </ActiveLink>
 
-        <ActiveLink activeClassName={active} href="/contact">
-          <a className={nav_link}>Contact</a>
-        </ActiveLink>
+          <ActiveLink activeClassName={active} href="/contact">
+            <a className={nav_link}>Contact</a>
+          </ActiveLink>
+        </div>
+      </nav>
 
-      </div>
-    </nav>
+      {/* Mobile only < 768px */}
+      <nav className={nav_mobile}>
+        <div id={nav_item}>
+          <ActiveLink activeClassName={active} href="/">
+            <a className={nav_link}>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={active} href="/projects">
+            <a className={nav_link}>Projects</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={active} href="/skills">
+            <a className={nav_link}>Skills</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={active} href="/about">
+            <a className={nav_link}>About Me</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={active} href="/contact">
+            <a className={nav_link}>Contact</a>
+          </ActiveLink>
+        </div>
+      </nav>
+    </>
   )
 }
 
