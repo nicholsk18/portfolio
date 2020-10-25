57651c729fffae0fbfa4257feb7c088d92056f50
Karson <nicholsk18@gmail.com>
2020-10-24 23:46:26 -0500

    working on skill section

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
+import {
+  skills_container,
+  skill,
+  skill_offset,
+} from '../../public/assets/css/skills.module.css'
 
 const Skills = () => {
   return (
@@ -9,6 +14,52 @@ const Skills = () => {
         <title>Karson Web Developer Skills | Karson Nichols</title>
       </Head>
       <PageHeader text="Skills" />
+
+      <div className={skills_container}>
+        <div className={skill}>
+          <img src="/assets/icons/react.png" alt="" />
+        </div>
+        <div className={`${skill} ${skill_offset}`}>
+          <img src="/assets/icons/js.png" alt="" />
+        </div>
+        <div className={skill}>
+          <img src="/assets/icons/css.png" alt="" />
+        </div>
+        <div className={`${skill} ${skill_offset}`}>
+          <img src="/assets/icons/html.png" alt="" />
+        </div>
+        <div className={skill}>
+          <img src="/assets/icons/php.png" alt="" />
+        </div>
+      </div>
+
+      <div className={skills_container}>
+        <div className={skill}>
+          <img src="/assets/icons/nodejs.png" alt="" />
+        </div>
+        <div className={`${skill} ${skill_offset}`}>
+          <img src="/assets/icons/scss.png" alt="" />
+        </div>
+        <div className={skill}>
+          <img src="/assets/icons/nextjs.png" alt="" />
+        </div>
+        <div className={`${skill} ${skill_offset}`}>
+          <img src="/assets/icons/wordpress.png" alt="" />
+        </div>
+        <div className={skill}>
+          <img src="/assets/icons/bootstrap.png" alt="" />
+        </div>
+      </div>
+
+      <div className={skills_container}>
+        <div className={skill}>
+          <img src="/assets/icons/jquery.png" alt="" />
+        </div>
+        <div className={`${skill} ${skill_offset}`}></div>
+        <div className={skill}>icon box</div>
+        <div className={`${skill} ${skill_offset}`}></div>
+        <div className={skill}>icon box</div>
+      </div>
     </Layout>
   )
 }
