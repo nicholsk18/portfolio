92567709dd99de9fb726d0bff28076b185d0593b
Karson <nicholsk18@gmail.com>
2020-10-25 21:52:48 -0500

    fix nav to only have one element

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -12,59 +12,32 @@ import {
 
 const Nav = () => {
   return (
-    <>
-      <nav id={navbar}>
-        <h2 id={nav_title}>Karson Nichols</h2>
-        <p id={nav_sub_title}>Web Developer</p>
+    <nav id={navbar}>
+      <h2 id={nav_title}>Karson Nichols</h2>
+      <p id={nav_sub_title}>Web Developer</p>
 
-        <div id={nav_item}>
-          <ActiveLink activeClassName={active} href="/">
-            <a className={nav_link}>Home</a>
-          </ActiveLink>
+      <div id={nav_item}>
+        <ActiveLink activeClassName={active} href="/">
+          <a className={nav_link}>Home</a>
+        </ActiveLink>
 
-          <ActiveLink activeClassName={active} href="/projects">
-            <a className={nav_link}>Projects</a>
-          </ActiveLink>
+        <ActiveLink activeClassName={active} href="/projects">
+          <a className={nav_link}>Projects</a>
+        </ActiveLink>
 
-          <ActiveLink activeClassName={active} href="/skills">
-            <a className={nav_link}>Skills</a>
-          </ActiveLink>
+        <ActiveLink activeClassName={active} href="/skills">
+          <a className={nav_link}>Skills</a>
+        </ActiveLink>
 
-          <ActiveLink activeClassName={active} href="/about">
-            <a className={nav_link}>About Me</a>
-          </ActiveLink>
+        <ActiveLink activeClassName={active} href="/about">
+          <a className={nav_link}>About Me</a>
+        </ActiveLink>
 
-          <ActiveLink activeClassName={active} href="/contact">
-            <a className={nav_link}>Contact</a>
-          </ActiveLink>
-        </div>
-      </nav>
-
-      {/* Mobile only < 768px */}
-      <nav className={nav_mobile}>
-        <div id={nav_item}>
-          <ActiveLink activeClassName={active} href="/">
-            <a className={nav_link}>Home</a>
-          </ActiveLink>
-
-          <ActiveLink activeClassName={active} href="/projects">
-            <a className={nav_link}>Projects</a>
-          </ActiveLink>
-
-          <ActiveLink activeClassName={active} href="/skills">
-            <a className={nav_link}>Skills</a>
-          </ActiveLink>
-
-          <ActiveLink activeClassName={active} href="/about">
-            <a className={nav_link}>About Me</a>
-          </ActiveLink>
-
-          <ActiveLink activeClassName={active} href="/contact">
-            <a className={nav_link}>Contact</a>
-          </ActiveLink>
-        </div>
-      </nav>
-    </>
+        <ActiveLink activeClassName={active} href="/contact">
+          <a className={nav_link}>Contact</a>
+        </ActiveLink>
+      </div>
+    </nav>
   )
 }
 
