083a8f2477a726aac149a0c6c06cfc960daedf5c
Karson Nichols <nicholsk18@gmail.com>
2020-10-27 06:59:25 -0500

    tags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,27 @@
-// import { Component } from 'react'
+import React, { Component } from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
 import '../../public/assets/css/index.css'
 
 export default function App({ Component, pageProps }) {
-  return (
+
+    return (
     <div>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154033556-1"></script>
+          <script dangerouslySetInnerHTML={{
+              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154033556-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154033556-1');
+</script>
+`
+          }}></script>
       </Head>
       <Component {...pageProps} />
     </div>
