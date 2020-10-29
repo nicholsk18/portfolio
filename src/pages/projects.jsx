7516fb4aa4246fd33cb81604aab9da863b7c0a7c
Karson Nichols <nicholsk18@gmail.com>
2020-10-29 14:03:12 -0500

    working on projects section

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
+import {
+    projects_container,
+    project
+} from '../../public/assets/css/projects.module.css'
 
 const Projects = () => {
   return (
@@ -9,6 +13,12 @@ const Projects = () => {
         <title>Karson Project Section | Karson Nichols</title>
       </Head>
       <PageHeader text="Projects" />
+
+      <div className={projects_container}>
+        <div className={project}>
+            <img src="/assets/images/projects/willcannon-project.png" alt=""/>
+        </div>
+      </div>
     </Layout>
   )
 }
