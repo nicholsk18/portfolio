5a063b04de20128c32191b9fa4727bc119f7ad7c
Karson Nichols <nicholsk18@gmail.com>
2020-11-03 09:29:47 -0600

    responsive skill box design?

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -5,6 +5,7 @@ import {
   skills_container,
   skill,
   skill_offset,
+    skills_offset_container
 } from '../../public/assets/css/skills.module.css'
 
 const Skills = () => {
@@ -19,49 +20,61 @@ const Skills = () => {
         <div className={skill}>
           <img src="/assets/icons/react.png" alt="" />
         </div>
-        <div className={`${skill} ${skill_offset}`}>
-          <img src="/assets/icons/js.png" alt="" />
-        </div>
+
         <div className={skill}>
           <img src="/assets/icons/css.png" alt="" />
         </div>
-        <div className={`${skill} ${skill_offset}`}>
-          <img src="/assets/icons/html.png" alt="" />
-        </div>
+
         <div className={skill}>
           <img src="/assets/icons/php.png" alt="" />
         </div>
+      </div>
+
+      <div className={skills_offset_container}>
+          <div className={`${skill} ${skill_offset}`}>
+              <img src="/assets/icons/js.png" alt="" />
+          </div>
+          <div className={`${skill} ${skill_offset}`}>
+              <img src="/assets/icons/html.png" alt="" />
+          </div>
       </div>
 
       <div className={skills_container}>
         <div className={skill}>
           <img src="/assets/icons/nodejs.png" alt="" />
         </div>
-        <div className={`${skill} ${skill_offset}`}>
-          <img src="/assets/icons/scss.png" alt="" />
-        </div>
         <div className={skill}>
           <img src="/assets/icons/nextjs.png" alt="" />
-        </div>
-        <div className={`${skill} ${skill_offset}`}>
-          <img src="/assets/icons/wordpress.png" alt="" />
         </div>
         <div className={skill}>
           <img src="/assets/icons/bootstrap.png" alt="" />
         </div>
       </div>
 
+
+        <div className={skills_offset_container}>
+            <div className={`${skill} ${skill_offset}`}>
+                <img src="/assets/icons/scss.png" alt="" />
+            </div>
+            <div className={`${skill} ${skill_offset}`}>
+                <img src="/assets/icons/wordpress.png" alt="" />
+            </div>
+        </div>
+
       <div className={skills_container}>
         <div className={skill}>
           <img src="/assets/icons/jquery.png" alt="" />
         </div>
-        <div className={`${skill} ${skill_offset}`}>
-          <img src="/assets/icons/java.png" alt="" />
-        </div>
         <div className={skill}>icon box</div>
-        <div className={`${skill} ${skill_offset}`}></div>
         <div className={skill}>icon box</div>
       </div>
+
+        <div className={skills_offset_container}>
+            <div className={`${skill} ${skill_offset}`}>
+                <img src="/assets/icons/java.png" alt="" />
+            </div>
+            <div className={`${skill} ${skill_offset}`}></div>
+        </div>
     </Layout>
   )
 }
